61a112ab027e93e0e65a237cc62caae89f729a8c
Kenan Balija <kenanbalija.dev@gmail.com>
2019-01-28 07:22:55 +0100

    fiished

--- src/components/SongDetail.js
+++ src/components/SongDetail.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-class SongDetail extends React.Component {
-  render() {
-    return (
-      <div>SongDetail</div>
-    )
+const SongDetail = ({ song }) => {
+  if (!song){
+    return <div>Select a song</div>;
   }
+  return (
+    <div>
+      <h3>Details for</h3>
+      <p>Title: {song.title}</p>
+      <p>Duration: {song.duration}</p>
+    </div>
+  )
 }
 
-export default SongDetail;
+const mapStateToProps = (state) => {
+  return { song: state.selectedSong }
+}
+
+export default connect(mapStateToProps)(SongDetail);
